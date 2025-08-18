fac9639bf5eaa57656990397d8bb743344be365a
amittkulkarni <amitcoolkarni@outlook.com>
2025-08-18 17:06:18 +0530

    Building fat JAR

---
+++ src/main/java/com/riftlink/p2p/Main.java
@@ -0,0 +1,10 @@
+package com.riftlink.p2p;
+
+/**
+ * A workaround launcher class for creating a runnable fat JAR.
+ */
+public class Main {
+    public static void main(String[] args) {
+        RiftLinkApp.main(args);
+    }
+}
